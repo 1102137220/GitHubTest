ae4c583e135263646b0a14052f06672a9383632f
1102137220 <1102137220@gm.kuas.edu.tw>
2016-03-03 14:59:07 +0800

    add hello

---
+++ WebApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class sdasda
+    {
+        public static void main()
+        {
+            System.Console.WriteLine("hi");
+            System.Console.ReadLine();
+        }
+    }
+}
